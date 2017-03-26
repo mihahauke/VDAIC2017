4f819ae24a83bd4faa1aefabc201ae5bc92713e4
michał <kempka.michal@gmail.com>
2017-03-26 15:58:20 +0200

    Removed separate run and build scripts for host.

--- host/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker build -t cig2017_host .
--- host/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker  run -p 5029 --net=host -t --rm --name cig2017_host \
--e DISPLAY=$DISPLAY  cig2017_host "$@"
-
