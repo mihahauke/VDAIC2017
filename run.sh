ee5276a867c9bd97a5dbb96d633cfe9e7d631edd
michał <kempka.michal@gmail.com>
2017-03-27 12:26:30 +0200

    Added warning that gui might not work.

--- run.sh
+++ run.sh
@@ -9,5 +9,10 @@ fi
 image_tag="cig2017_`basename $DIRECTORY`"
 container_name=${image_tag}
 
+
+if [ "`uname`" != "Linux" ]; then
+  echo "WARNING: GUI forwarding in Docker was tested only on a linux host."
+fi
+
 docker  run -p 5029 --net=host -ti --rm --name ${container_name} \
     -e DISPLAY=${DISPLAY}  ${image_tag} "${@:2}"
