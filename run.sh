5826d340aa3485dea663f75440717517b84bf233
michał <kempka.michal@gmail.com>
2017-03-28 17:20:22 +0200

    Run can spawn multiple containers from the same image.

--- run.sh
+++ run.sh
@@ -24,6 +24,14 @@ fi
 image_tag="cig2017_`basename $DIRECTORY`"
 container_name=${image_tag}
 
+run_version=0
+while [[ ! -z `docker ps --format '{{.Names}}'|grep "^${container_name}$"`  ]]
+do
+    echo "WARNING: '${container_name}' is already a running docker container. Trying to run '${image_tag}_${run_version}'."
+    run_version=$(($run_version+1))
+    container_name=${image_tag}_${run_version}
+done
+
 
 if [ "`uname`" != "Linux" ]; then
   echo "WARNING: GUI forwarding in Docker was tested only on a linux host."
