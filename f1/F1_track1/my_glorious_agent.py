ae0a6a1297e0bccffac9630f4424c7027138dffd
michał <kempka.michal@gmail.com>
2017-03-29 20:48:55 +0200

    Removed accidental final logs from f1.

--- f1/F1_track1/my_glorious_agent.py
+++ f1/F1_track1/my_glorious_agent.py
@@ -29,20 +29,7 @@ while not game.is_episode_finished():
         #continue;
 
     runner.step()
-    # Log your frags every ~5 seconds
-    print("Frags:", game.get_game_variable(GameVariable.FRAGCOUNT))
+    
 
-print(20*"#")
-print("Match results:")
-print("My frags: " + str(game.get_game_variable(GameVariable.FRAGCOUNT)))
-print()
-print("Player1: " + str(game.get_game_variable(GameVariable.PLAYER1_FRAGCOUNT)))
-print("Player2: " + str(game.get_game_variable(GameVariable.PLAYER2_FRAGCOUNT)))
-print("Player3: " + str(game.get_game_variable(GameVariable.PLAYER3_FRAGCOUNT)))
-print("Player4: " + str(game.get_game_variable(GameVariable.PLAYER4_FRAGCOUNT)))
-print("Player5: " + str(game.get_game_variable(GameVariable.PLAYER5_FRAGCOUNT)))
-print("Player6: " + str(game.get_game_variable(GameVariable.PLAYER6_FRAGCOUNT)))
-print("Player7: " + str(game.get_game_variable(GameVariable.PLAYER7_FRAGCOUNT)))
-print("Player8: " + str(game.get_game_variable(GameVariable.PLAYER8_FRAGCOUNT)))
 
 game.close()
